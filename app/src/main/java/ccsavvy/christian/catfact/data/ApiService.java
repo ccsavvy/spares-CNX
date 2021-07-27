38a119174b2488c6ebeaeb3f3fc9d3ba441b97ae
Christian Lumapac <christianlumapac@gmail.com>
2021-07-28 01:22:18 +0800

    Update ApiService.java

--- app/src/main/java/ccsavvy/christian/catfact/data/ApiService.java
+++ app/src/main/java/ccsavvy/christian/catfact/data/ApiService.java
@@ -1,11 +1,12 @@
 package ccsavvy.christian.catfact.data;
 
+import ccsavvy.christian.catfact.model.Facts;
 import ccsavvy.christian.catfact.model.NinjaCatFacts;
 import io.reactivex.Single;
 import retrofit2.http.GET;
 
 interface ApiService {
-    @GET("/fact")
-    Single<NinjaCatFacts> getFact();
+    @GET("/facts/random")
+    Single<Facts> getFact();
 }
 
